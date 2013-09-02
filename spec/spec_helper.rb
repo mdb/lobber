d9ebbfa99af992ced1d0ea00104d62cd0fc55c48
Mike Ball <mikedball@gmail.com>
2013-09-02 07:59:47 -0400

    Added exit_with_code matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'simplecov'
 require 'rspec'
 SimpleCov.start
 
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 require 'lob'
 
 RSpec.configure do |config|
