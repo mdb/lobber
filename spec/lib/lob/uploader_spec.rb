79562d3ac2c09d9daecd21aff5b7ce09c6fa335f
Mike Ball <mikedball@gmail.com>
2013-09-02 08:00:45 -0400

    Improved some specs

--- spec/lib/lob/uploader_spec.rb
+++ spec/lib/lob/uploader_spec.rb
@@ -4,6 +4,9 @@ describe "Lob::Upload" do
   before :each do
     Fog.mock!
     @uploader = Lob::Uploader.new 'spec'
+    @uploader.stub(:aws_access_key).and_return 'fake key'
+    @uploader.stub(:aws_secret_key).and_return 'fake key'
+    @uploader.stub(:fog_directory).and_return 'fog directory'
   end
 
   after :each do
@@ -48,14 +51,24 @@ describe "Lob::Upload" do
         "spec/lib/lob/" => :directory,
         "spec/lib/lob/uploader_spec.rb" => "content",
         "spec/lib/lob_spec.rb" => "content",
-        "spec/spec_helper.rb" => "content"
+        "spec/spec_helper.rb" => "content",
+        "spec/support/" => :directory,
+        "spec/support/matchers/" => :directory,
+        "spec/support/matchers/exit_with_code.rb" => "content"
       )
     end
   end
 
   # pending
   describe "#bucket" do
-    it "" do
+    context "its behavior when a @bucket instance variable has been defined" do
+      it "" do
+      end
+    end
+
+    context "its behavior when a @bucket instance variable has not been defined" do
+      it "" do
+      end
     end
   end
 
@@ -77,17 +90,16 @@ describe "Lob::Upload" do
 
   describe "#create_directory" do
     it "creates an s3 directory" do
-      @uploader.bucket.files(:create).should_receive key: 'foo', public: true
-      @uploader.stub(:aws_access_key).and_return 'fake_key'
-      @uploader.stub(:aws_secret_key).and_return 'fake_key'
-      @uploader.stub(:fog_directory).and_return 'fog_directory'
+      @uploader.bucket.files.should_receive(:create).with(key: 'foo', public: true)
       @uploader.create_directory "foo"
     end
   end
 
-  # pending
   describe "#create_file" do
     it "creates an s3 file" do
+      File.stub(:open).and_return 'content'
+      @uploader.bucket.files.should_receive(:create).with(key: 'foo', public: true, body: 'content')
+      @uploader.create_file "foo"
     end
   end
 
@@ -110,13 +122,35 @@ describe "Lob::Upload" do
   describe "#verify_env_variables" do
     context "when one of the required environment variables is absent" do
       before :each do
-        ENV.stub(:[]).with("AWS_SECRET_KEY").and_return("secret_key")
-        ENV.stub(:[]).with("FOG_DIRECTORY").and_return("fog_directory")
+        ENV.stub(:[]).and_return(nil)
+        ENV.stub(:[]).and_return(nil)
+        ENV.stub(:[]).and_return(nil)
+      end
+
+      it "exists with an exit code of 1" do
+        lambda { @uploader.verify_env_variables }.should exit_with_code(1)
       end
 
       it "it reports that the missing env variable is required" do
-        Kernel.should_receive(:puts)#.with 'AWS_ACCESS_KEY required'
-        Kernel.should_receive(:exit).with 1
+        Kernel.should_receive(:puts).with 'AWS_ACCESS_KEY required'
+        lambda { @uploader.verify_env_variables }.call
+      end
+    end
+
+    context "when all of the required environment variables are defined" do
+      before :each do
+        ENV.stub(:[]).with("AWS_SECRET_KEY").and_return 'secret key'
+        ENV.stub(:[]).with("AWS_ACCESS_KEY").and_return 'access key'
+        ENV.stub(:[]).with("FOG_DIRECTORY").and_return 'fog directory'
+      end
+
+      it "does not exit with an exit code of 1" do
+        lambda { @uploader.verify_env_variables }.should_not exit_with_code(1)
+      end
+
+      it "it reports that the missing env variable is required" do
+        Kernel.stub(:exit).and_return true
+        @uploader.should_not receive(:puts).with 'AWS_ACCESS_KEY required'
         lambda { @uploader.verify_env_variables }
       end
     end
@@ -130,22 +164,28 @@ describe "Lob::Upload" do
 
   describe "#aws_access_key" do
     it "returns the value of the AWS_ACCESS_KEY environment variable" do
-      ENV.stub(:[]).with("AWS_ACCESS_KEY").and_return('fake key')
-      @uploader.aws_access_key.should eq 'fake key'
+      some_uploader = Lob::Uploader.new 'foo'
+      ENV.stub(:[])
+      ENV.should_receive(:[]).with 'AWS_ACCESS_KEY'
+      some_uploader.aws_access_key
     end
   end
 
   describe "#aws_secret_key" do
     it "returns the value of the AWS_ACCESS_KEY environment variable" do
-      ENV.stub(:[]).with("AWS_SECRET_KEY").and_return('fake key')
-      @uploader.aws_secret_key.should eq 'fake key'
+      some_uploader = Lob::Uploader.new 'foo'
+      ENV.stub(:[])
+      ENV.should_receive(:[]).with 'AWS_SECRET_KEY'
+      some_uploader.aws_secret_key
     end
   end
 
   describe "#fog_directory" do
     it "returns the value of the FOG_DIRECTORY environment variable" do
-      ENV.stub(:[]).with("FOG_DIRECTORY").and_return('fake directory')
-      @uploader.fog_directory.should eq 'fake directory'
+      some_uploader = Lob::Uploader.new 'foo'
+      ENV.stub(:[])
+      ENV.should_receive(:[]).with 'FOG_DIRECTORY'
+      some_uploader.fog_directory
     end
   end
 end
