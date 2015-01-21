531069c1f20b2c118dedefa19e987abc2c0c2bc6
Mike Ball <mikedball@gmail.com>
2015-01-21 16:33:42 -0500

    version bump to 0.0.5

--- lib/lobber/version.rb
+++ lib/lobber/version.rb
@@ -1,3 +1,3 @@
 module Lobber
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
--- spec/lib/lobber/cli_spec.rb
+++ spec/lib/lobber/cli_spec.rb
@@ -66,7 +66,7 @@ describe Lobber::CLI do
     subject(:usage) { cli.usage }
 
     it "displays version info, GitHub info, and help" do
-      expect(cli).to receive(:say).with('Lobber 0.0.4')
+      expect(cli).to receive(:say).with('Lobber 0.0.5')
       expect(cli).to receive(:say).with('https://github.com/mdb/lob')
       expect(cli).to receive(:say).with("\n")
       expect(cli).to receive(:help)
